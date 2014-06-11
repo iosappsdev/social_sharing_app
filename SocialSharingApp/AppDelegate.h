3687ebc8a4c3baba41621133710648f71e48e485
CtecTeacher <ctecteacher@H8-T-607853.local>
2014-06-10 18:19:07 -0700

    Initial Commit

---
+++ SocialSharingApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SocialSharingApp
+//
+//  Created by CtecTeacher on 6/10/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SocialSharingApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SocialSharingApp
+//
+//  Created by CtecTeacher on 6/10/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
