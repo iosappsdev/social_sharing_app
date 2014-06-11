10de3d4feb6250e0df18e6314cc934d5f21dd251
iOS Apps Development <ricardo@iosappsdev.org>
2014-06-10 22:20:43 -0700

    Cleaned Up Code

--- SocialSharingApp/ViewController.m
+++ SocialSharingApp/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  SocialSharingApp
 //
-//  Created by CtecTeacher on 6/10/14.
+//  Created by Ricardo Ruiz on 6/10/14.
 //  Copyright (c) 2014 iOS Apps Development. All rights reserved.
 //
 
@@ -12,11 +12,9 @@
 @interface ViewController (){
     
     UIImage *selectedImage;
-    
 }
 @property (weak, nonatomic) IBOutlet UITextView *myTextView;
 @property (weak, nonatomic) IBOutlet UIImageView *myImageView;
-
 
 - (IBAction)postToFacebook:(id)sender;
 - (IBAction)postToTwitter:(id)sender;
@@ -33,18 +31,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
     myTextView.text = @"Standard Text";
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)postToFacebook:(id)sender {
     
+    // Check to see if ServiceType is Available
     if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
         SLComposeViewController *controller = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
         
@@ -52,31 +44,25 @@
         [controller addImage:selectedImage];
         [self presentViewController:controller animated:YES completion:^{NSLog(@"Present View Controller Called");}];
         
-    }else{
+    } else {
+    	
         UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Alert" message:@"Facebook not available" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
         
         [alert show];
     }
-    
-
-    
 }
 
 - (IBAction)postToTwitter:(id)sender {
 
     if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]){
-    
         SLComposeViewController *tweetSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
         
         [ tweetSheet setInitialText:myTextView.text];
-        
         [self presentViewController:tweetSheet animated:YES completion:^{NSLog(@"Present View Controller Called.");}];
         
-    }else{
+    } else {
     
-        
         UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Alert" message:@"Twitter not available" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-        
         [alert show];
     }
      
@@ -86,82 +72,39 @@
 - (IBAction)selectPhoto:(id)sender {
     
     UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
-    
     imagePicker.delegate = self;
-    
-    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary; // Select PhotoLibrary
     
     [self presentViewController:imagePicker animated:YES completion:nil];
-    
-    
-    
 }
 
 - (IBAction)takePhoto:(id)sender {
+	
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-   
-    
+    	
     UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
-    
-    
     imagePicker.delegate = self;
-    
     imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
     
     [self presentViewController:imagePicker animated:YES completion:nil];
-    }else{
+    } else {
     
         UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Message" message:@"Camera not available" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-        
         [alert show];
-        
-        
     }
-    
-    
-    }
+}
 
 - (IBAction)activityView:(id)sender {
     
-   UIActivityViewController *controller = [[UIActivityViewController alloc]initWithActivityItems:@[selectedImage]
-                                           
-                                                                        applicationActivities:nil];
-    
+   UIActivityViewController *controller = [[UIActivityViewController alloc]initWithActivityItems:@[selectedImage]applicationActivities:nil];
     [self presentViewController:controller animated:YES completion:nil];
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
-
-
 
 -(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
 
     [picker dismissViewControllerAnimated:YES completion:nil];
-    
+
     selectedImage = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
-
     myImageView.image = selectedImage;
-    
 }
-    
-    
-
-    
-    
-
 @end
